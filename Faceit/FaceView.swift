29518692dfff3215155fd6f328f176d6a52f1a3c
Alana Anderson <alanaanderson@Alanas-MacBook-Air.local>
2016-08-10 11:58:01 -0700

    first commit

---
+++ Faceit/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  Faceit
+//
+//  Created by Alana Anderson on 8/10/16.
+//  Copyright © 2016 Alana Anderson. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
